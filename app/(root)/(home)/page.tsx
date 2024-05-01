f191dd87f3100d65be3765f1719adcdd8670f283
Mritunjay Pramanick <mritunjay.softech@gmail.com>
2024-05-02 01:14:07 +0530

    second Change

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -10,7 +10,7 @@ const Home = () => {
     <section className='flex size-full flex-col gap-10 text-white'>
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className='glassmorphism max-w-[270px] rounded text-center text-base py-2 font-normal'>Upcoming Meeting at: 12 : 30 AM</h2>
+          <h2 className='glassmorphism max-w-[270px] rounded text-center text-base py-2 font-normal'>Upcoming Meeting at: 01 : 30 AM</h2>
           <div className="flex flex-col gap-2">
             <h1 className='text-4xl font-extrabold lg:text-7xl'>{`${time}`}</h1>
             <p className='text-lg font-medium text-sky-1 lg:text-2xl'>{`${date}`}</p>
